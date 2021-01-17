a87985cf0d728bdf233bd70251fb06740ed4140f
Raúl Chirinos <raulstuf@gmail.com>
2021-01-17 11:01:55 -0400

    Clean workspace

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import config from "../config/config"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({ c }) => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <h1>{c("titleName")}</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </>
 )
 
 export default config(IndexPage)
