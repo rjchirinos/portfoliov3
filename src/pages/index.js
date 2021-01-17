ee19b0c813e6b69366f870e29d48c853e5699f37
Raúl Chirinos <raulstuf@gmail.com>
2021-01-17 13:09:35 -0400

    Cleaning before

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import config from "../config/config"
 import SEO from "../components/seo"
+import App from "../sections/App/App"
 
 import "./index.scss"
-import Box from "../components/Box"
 
 const IndexPage = ({ c }) => (
   <>
     <SEO title="Home" />
-    <h1>{c("titleName")}</h1>
-    <p className="n1">Welcome to your new Gatsby site.</p>
-    <p className="n2">Welcome to your new Gatsby site.</p>
-    <p className="n2">Welcome to your new Gatsby site.</p>
-    <Box />
+    <App />
   </>
 )
 
-export default config(IndexPage)
+export default IndexPage
