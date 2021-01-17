8eff29a12513c95541e2ae3f31a59ed6e47a772d
Raúl Chirinos <raulstuf@gmail.com>
2021-01-17 13:02:32 -0400

    Setting theme

---
+++ src/components/Box.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Box.scss"
+
+const Box = () => {
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default Box;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,17 @@ import { Link } from "gatsby"
 import config from "../config/config"
 import SEO from "../components/seo"
 
+import "./index.scss"
+import Box from "../components/Box"
+
 const IndexPage = ({ c }) => (
   <>
     <SEO title="Home" />
     <h1>{c("titleName")}</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p className="n1">Welcome to your new Gatsby site.</p>
+    <p className="n2">Welcome to your new Gatsby site.</p>
+    <p className="n2">Welcome to your new Gatsby site.</p>
+    <Box />
   </>
 )
 
