abae64943bd73d27492bf0eca7ed61f31c1f852a
Raúl Chirinos <raulstuf@gmail.com>
2021-01-22 10:31:41 -0400

    Add button component

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
 import Navbar from '../../components/Navbar/Navbar';
 import SocialRow from '../../components/SocialRow/SocialRow';
 import config from "../../config/config"
@@ -18,6 +19,7 @@ const Header = ({ c }) => {
                 <h1 className="header-title">{c("titleName")}</h1>
                 <h2 style={{ marginBottom: "1rem" }} className="header-subtitle">{c("subtitle.first")} <span className="tint">{c("subtitle.second")}</span></h2>
                 <SocialRow iconSize={!isMedium ? "32px" : "24px"} margin={!isMedium ? "30px" : "20px"} />
+                <Button style={{ marginTop: "1rem" }}>Say hello!</Button>
             </div>
         </section>
     );
