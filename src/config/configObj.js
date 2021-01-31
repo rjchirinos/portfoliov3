2cc04759a637365a3a2248ade61f541c80a18ca3
Raúl Chirinos <raulstuf@gmail.com>
2021-01-31 12:36:41 -0400

    Add about section

--- src/config/configObj.js
+++ src/config/configObj.js
@@ -4,5 +4,11 @@ export const configObj = {
     subtitle: {
         first: "I create stunnig",
         second: "webapps"
-    }
+    },
+    about: {
+        first: "I'm Raúl, I am a Web Developer based in Punto Fijo, Venezuela, and I dedicate to",
+        second: "develop effective solutions",
+        third: "to adapt your brand or business to the digital world through code, providing you all the opportunities offered by having a greater reach and using the most advanced and popular technologies."
+    },
+    aboutP2: "Technologies I have worked with:"
 }
--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
+import profile from "../../images/photo.jpg"
+import config from "../../config/config";
 
-const About = () => {
+const technologies = ["React", "Node.js", "Express.js", "MongoDB", "MySQL", "Gatbsy", "Jest", "Expo"]
+
+const About = ({ c }) => {
     return (
         <section>
             <Layout title="About">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt tenetur quisquam quo repellat necessitatibus ipsa, cum tempora numquam dignissimos ipsum dolore ab consectetur optio natus commodi sed? Nulla, voluptatem perspiciatis.
+                <div className="py-4 md:pb-40 flex flex-col items-center md:flex-row-reverse">
+                    <div className="bg-primary rounded-full w-4/5 max-w-xs mb-4">
+                        <img className="transition duration-300 rounded-full opacity-40 hover:opacity-100" src={profile} alt="profile" />
+                    </div>
+                    <div className="md:w-4/5 md:pr-12">
+                        <p>{c("about.first")} <span className="text-primary">{c("about.second")}</span> {c("about.third")}</p>
+                        <p className="mt-2">
+                            <span>{c("aboutP2")}</span>
+                            <ul className="list-disc col-count-2 mt-2">
+                                {technologies.map(tech => (
+                                    <li>{tech}</li>
+                                ))}
+                            </ul>
+                        </p>
+                    </div>
+                </div>
             </Layout>
         </section>
     );
 }
 
-export default About;
+export default config(About);
