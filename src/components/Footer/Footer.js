02c3a922885901e819c91b144b1ab028fe4f1305
Raúl Chirinos <raulstuf@gmail.com>
2021-01-31 19:11:45 -0400

    Fix footer width

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="w-screen bg-background-light">
+        <div className="w-full bg-background-light">
             <p className="m-auto py-12 text-center">
                 <span className="text-primary">Raúl Chirinos</span> © Copyright 2021
             </p>
